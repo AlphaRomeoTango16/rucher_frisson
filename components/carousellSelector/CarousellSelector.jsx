0d2c9104a9ae546218289062ad2fa24489a8d75a
Arthur Della Faille <arthur16.dellafaille@gmail.com>
2022-05-11 09:35:53 +0200

    modifications

---
+++ components/carousellSelector/CarousellSelector.jsx
@@ -0,0 +1,93 @@
+import React, {useRef} from 'react';
+import styled from 'styled-components';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faChevronLeft} from '@fortawesome/free-solid-svg-icons';
+import {faChevronRight} from '@fortawesome/free-solid-svg-icons';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import propTypes from 'prop-types';
+
+import 'swiper/css';
+
+CarousellSelector.propTypes = {
+    productType: propTypes.array.isRequired,
+    setSwiperIndex: propTypes.func.isRequired
+}
+
+export default function CarousellSelector({productType, setSwiperIndex}) {
+    const swiperRef = useRef(null);
+
+    return (
+        <RadialGradient>
+            <CarousellSelectorContainer>
+                <ChevronButton>
+                    <ChevronIconLeft onClick={() => swiperRef.current.swiper.slidePrev()} icon={faChevronLeft}/>
+                </ChevronButton>
+                <Swiper
+                    ref={swiperRef}
+                    spaceBetween={10}
+                    slidesPerView={1}
+                    onActiveIndexChange={(swiperCore) => setSwiperIndex(swiperCore.activeIndex)}
+                >
+                    {productType.map((category, index) => {
+                        return (
+                            <SwiperSlide key={index}>
+                                <ProductTypeTitle>{category.name}</ProductTypeTitle>
+                            </SwiperSlide>
+                        )
+                    })}
+                </Swiper>
+                <ChevronButton>
+                    <ChevronIconRight onClick={() => swiperRef.current.swiper.slideNext()} icon={faChevronRight}/>
+                </ChevronButton>
+            </CarousellSelectorContainer>
+        </RadialGradient>
+    )
+}
+
+const RadialGradient = styled.div`
+    display: flex;
+    width: 200px;
+    height: 70px;
+    justify-content: center;
+    align-items: center;
+    background: radial-gradient(60% 40% at center, black, grey, white)
+`
+
+const CarousellSelectorContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: white;
+    width: 100%;
+    height: 30px;
+`
+
+const ChevronButton = styled.div`
+    display: flex;
+    width: 20px;
+`
+
+const ChevronIconRight = styled(FontAwesomeIcon)`
+    display: flex;
+    width: 10px;
+    margin-left: 10px;
+    margin-top: 0px;
+    margin-bottom: 0px;
+`
+
+const ChevronIconLeft = styled(FontAwesomeIcon)`
+    display: flex;
+    width: 10px;
+    margin-right: 10px;
+    margin-top: 0px;
+    margin-bottom: 0px;
+`
+
+const ProductTypeTitle = styled.p`
+    font-family: FiraSansCondensed-Light;
+    font-size: 20px;
+    text-align: center;
+    width: 100%;
+    margin-top: 0px;
+    margin-bottom: 0px;
+`
---
+++ components/displayProduct/Mosaic.jsx
@@ -0,0 +1,55 @@
+import React, {useState} from 'react';
+import styled from 'styled-components';
+import Card from '../card/Card';
+import propTypes from 'prop-types';
+
+Mosaic.propTypes = {
+    desktopDevice: propTypes.bool.isRequired,
+    mobileDevice: propTypes.bool.isRequired,
+    products: propTypes.array.isRequired,
+    swiperIndex: propTypes.number.isRequired
+}
+
+export default function Mosaic({products, swiperIndex}) {
+    const [mosaicFormat] = useState(true);
+
+    return (
+        <DisplayProductContainer>
+            <MosaicContainer>
+                {swiperIndex === 0 &&
+                products.filter((item) => item.category === 'Pollen').map((product, index) => {
+                    return (
+                        <Card
+                            key={index}
+                            product={product}
+                            mosaicFormat={mosaicFormat}
+                        />
+                    )
+                })}
+                {swiperIndex === 1 &&
+                products.filter((item) => item.category === 'Miel').map((product, index) => {
+                    return (
+                        <Card
+                            key={index}
+                            product={product}
+                            mosaicFormat={mosaicFormat}
+                        />
+                    )
+                })}
+            </MosaicContainer>
+        </DisplayProductContainer>
+
+    )
+}
+
+const DisplayProductContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+`
+
+const MosaicContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+`
